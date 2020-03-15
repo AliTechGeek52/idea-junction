bd380f7d1be06a20c965a60217adf27f41038223
syed sameer <sameer.shah12570@gmail.com>
2020-03-15 13:45:46 +0500

    Updates

---
+++ precache-manifest.3039900f045a4b66b34c1ee4cb1bcfa0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d45c10cde2eb5feb110624bca94f8ff4",
+    "url": "/AliTechGeek52/idea-junction.git/index.html"
+  },
+  {
+    "revision": "8174436e2ec6c94c865d",
+    "url": "/AliTechGeek52/idea-junction.git/static/css/main.72611654.chunk.css"
+  },
+  {
+    "revision": "0a6cf48e32c83638f079",
+    "url": "/AliTechGeek52/idea-junction.git/static/js/2.8c0c3a44.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/AliTechGeek52/idea-junction.git/static/js/2.8c0c3a44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8174436e2ec6c94c865d",
+    "url": "/AliTechGeek52/idea-junction.git/static/js/main.cea1d21d.chunk.js"
+  },
+  {
+    "revision": "216091da8172301b70f8",
+    "url": "/AliTechGeek52/idea-junction.git/static/js/runtime-main.a56502d7.js"
+  },
+  {
+    "revision": "6fc3e7c7d53acbc28ee0760b302c4712",
+    "url": "/AliTechGeek52/idea-junction.git/static/media/crowd_bg1.6fc3e7c7.jpg"
+  },
+  {
+    "revision": "c16ad4006d4fb46dc1628c185159fa66",
+    "url": "/AliTechGeek52/idea-junction.git/static/media/crowd_bg2.c16ad400.png"
+  },
+  {
+    "revision": "de0ac5b746749387a26916c63ebea4f7",
+    "url": "/AliTechGeek52/idea-junction.git/static/media/crowd_bg3.de0ac5b7.png"
+  },
+  {
+    "revision": "959e85697f73817cfefa887a4895c88a",
+    "url": "/AliTechGeek52/idea-junction.git/static/media/crowd_bg4.959e8569.png"
+  },
+  {
+    "revision": "9baaf6018dbe94f6389c9fbff214b45a",
+    "url": "/AliTechGeek52/idea-junction.git/static/media/map.9baaf601.png"
+  },
+  {
+    "revision": "e709d1e776a8c058e5f316bf24aa7c3b",
+    "url": "/AliTechGeek52/idea-junction.git/static/media/unfold-logo-dark.e709d1e7.png"
+  },
+  {
+    "revision": "cd693372a24c44a4c609efd182fd9f39",
+    "url": "/AliTechGeek52/idea-junction.git/static/media/unfold-logo-white.cd693372.png"
+  }
+]);
